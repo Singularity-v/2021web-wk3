cdac3d376920106ff64057737eb5ae9ca190981f
Crystal Wu <crystalwu.home@gmail.com>
2021-03-15 22:21:53 +0800

    Add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="text-center d-flex row flex-column">
+        <header className="header text-center d-flex row flex-column">
             <h2 className="text-white pt-5 pb-2">
                 NORDIC NEST Shopping Cart
             </h2>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import Header from "../components/Header"
 import ProductList from "../components/ProducList"
 import Footer from "../components/Footer"
+import NavBar from "../components/NavBar"
+
 function Home() {
   return (
     <div className="bg-color">
         <div className="container grid-container">
             <Header />
+            <NavBar/>
             <ProductList />
             <Footer />
         </div>
